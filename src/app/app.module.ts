3e2a91ca288a8e2793da11f9bedd230a8a14e6e3
Sachit Malhotra <sachitsac@gmail.com>
2017-07-28 14:41:04 +1000

    Initialise appolo client and add all the dependencies required to run appolo graphql with this app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ApolloModule } from 'apollo-angular';
 
 import { AppComponent } from './app.component';
+import { AppoloDataService, provideClient } from './services/appolo-data.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ApolloModule.forRoot( provideClient )
   ],
-  providers: [],
+  providers: [ AppoloDataService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/appolo-data.service.ts
+++ src/app/services/appolo-data.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { ApolloClient, createNetworkInterface } from 'apollo-client';
+
+// Create the client as outlined above
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'https://cortex.equiem.com.au/v1/graphql'
+  }),
+});
+
+export function provideClient(): ApolloClient {
+  return client;
+}
 
 @Injectable()
 export class AppoloDataService {
