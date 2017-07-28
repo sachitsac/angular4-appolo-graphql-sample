c55ddc52f7edc39e84de88a27b97445c2e19687e
Sachit Malhotra <sachitsac@gmail.com>
2017-07-28 15:02:49 +1000

    Remove Default angular cli generated html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppoloDataService } from './services/appolo-data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  tableData: any;
+
+  constructor( private ds: AppoloDataService ) {
+    this.ds.getData().subscribe( (res) => {
+      this.tableData = res.data;
+    });
+  }
 }
